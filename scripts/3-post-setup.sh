758304f1dc9e083d599dd883b3afe952b6691b41
Stelios Mitas <77465022+MitasTech@users.noreply.github.com>
2022-03-28 16:04:12 +0100

    Update 3-post-setup.sh

--- scripts/3-post-setup.sh
+++ scripts/3-post-setup.sh
@@ -72,7 +72,7 @@ elif [[ "${DESKTOP_ENV}" == "lxde" ]]; then
 elif [[ "${DESKTOP_ENV}" == "openbox" ]]; then
   systemctl enable lightdm.service
   if [[ "${INSTALL_TYPE}" == "FULL" ]]; then
-    # Set default lightdm-webkit2-greeter theme to Aether
+    # Set default lightdm-webkit2-greeter theme to Litarvan
     sed -i 's/^webkit_theme\s*=\s*\(.*\)/webkit_theme = litarvan #\1/g' /etc/lightdm/lightdm-webkit2-greeter.conf
     # Set default lightdm greeter to lightdm-webkit2-greeter
     sed -i 's/#greeter-session=example.*/greeter-session=lightdm-webkit2-greeter/g' /etc/lightdm/lightdm.conf
