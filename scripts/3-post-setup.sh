5428f53ab24bedbb72c4702f51abbd0d90db64b9
Stelios Mitas <77465022+MitasTech@users.noreply.github.com>
2022-03-28 15:30:34 +0100

    Update 3-post-setup.sh

--- scripts/3-post-setup.sh
+++ scripts/3-post-setup.sh
@@ -57,12 +57,10 @@ echo -ne "
 -------------------------------------------------------------------------
 "
 if [[ ${DESKTOP_ENV} == "kde" ]]; then
-  systemctl enable lightdm.service
-  if [[ "${INSTALL_TYPE}" == "FULL" ]]; then
-    # Set default lightdm-webkit2-greeter theme to aether
-    sed -i 's/^webkit_theme\s*=\s*\(.*\)/webkit_theme = aether #\1/g' /etc/lightdm/lightdm-webkit2-greeter.conf
-    # Set default lightdm greeter to lightdm-webkit2-greeter
-    sed -i 's/#greeter-session=example.*/greeter-session=lightdm-webkit2-greeter/g' /etc/lightdm/lightdm.conf
+  systemctl enable sddm.service
+  if [[ ${INSTALL_TYPE} == "FULL" ]]; then
+    echo [Theme] >>  /etc/sddm.conf
+    echo Current=Nordic >> /etc/sddm.conf
   fi
 
 elif [[ "${DESKTOP_ENV}" == "gnome" ]]; then
