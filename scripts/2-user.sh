757c70027b9b24df27a71a29e58b0968760bdf37
Stelios Mitas <77465022+MitasTech@users.noreply.github.com>
2022-04-05 14:40:25 +0300

    Update 2-user.sh

--- scripts/2-user.sh
+++ scripts/2-user.sh
@@ -83,8 +83,12 @@ if [[ $INSTALL_TYPE == "FULL" ]]; then
     cd .config
     dconf load / < full-backup
   fi
-fi
-
+# Theming DE if user chose FULL Awesome installation
+if [[ $INSTALL_TYPE == "FULL" ]]; then
+  if [[ $DESKTOP_ENV == "awesome" ]]; then
+    cd ~
+    git clone https://github.com/ChrisTitusTech/titus-awesome ~/.config/awesome
+  fi
 echo -ne "
 -------------------------------------------------------------------------
                     SYSTEM READY FOR 3-post-setup.sh
