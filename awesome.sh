c3d7efde985a87f177ecaeecef86d559f993b46a
Stelios Mitas <77465022+MitasTech@users.noreply.github.com>
2022-04-05 18:05:15 +0300

    Update awesome.sh

--- awesome.sh
+++ awesome.sh
@@ -17,17 +17,9 @@ echo -ne "
 sleep 5
 
 cd ~
-sed -n '/'$INSTALL_TYPE'/q;p' ~/ArchMitas/pkg-files/awesome.txt | while read line
-do
-  if [[ ${line} == '--END OF MINIMAL INSTALL--' ]]
-  then
-    # If selected installation type is FULL, skip the --END OF THE MINIMAL INSTALLATION-- line
-    continue
-  fi
-  echo "INSTALLING: ${line}"
-  sudo pacman -S --noconfirm --needed ${line}
-done
+sudo pacman -Sy awesome lightdm lightdm-gtk-greeter thunar lxsession rxvt-unicode alsa-utils pulseaudio pulseaudio-alsa wireless_tools zsh dunst rofi feh i3lock-fancy lightdm-webkit2-greeter lightdm-webkit-theme-aether lxappearance pnmixer qt5ct gsimplecal xautolock xclip network-manager-applet networkmanager-openvpn materia-theme polkit-gnome scrot flameshot ttf-roboto thunar-archive-plugin thunar-volman thunar-media-tags-plugin tumbler jq w3m geany nano viewnior pavucontrol qt5-styleplugins parcellite neofetch htop picom gtk2-perl xfce4-power-manager imagemagick playerctl xsettingsd obconf
 
+sudo -k
 
 if [[ ! $AUR_HELPER == none ]]; then
   cd ~
