2eb71cc65484dd39134d6f6e279594529ace4f40
Stelios Mitas <77465022+MitasTech@users.noreply.github.com>
2022-04-05 18:14:21 +0300

    Update 1-setup.sh

--- scripts/1-setup.sh
+++ scripts/1-setup.sh
@@ -75,17 +75,17 @@ echo -ne "
 "
 # sed $INSTALL_TYPE is using install type to check for MINIMAL installation, if it's true, stop
 # stop the script and move on, not installing any more packages below that line
-if [[ ! $DESKTOP_ENV == server ]]; then
-  sed -n '/'$INSTALL_TYPE'/q;p' $HOME/ArchMitas/pkg-files/pacman-pkgs.txt | while read line
-  do
-    if [[ ${line} == '--END OF MINIMAL INSTALL--' ]]; then
-      # If selected installation type is FULL, skip the --END OF THE MINIMAL INSTALLATION-- line
-      continue
-    fi
-    echo "INSTALLING: ${line}"
-    sudo pacman -S --noconfirm --needed ${line}
-  done
-fi
+#if [[ ! $DESKTOP_ENV == server ]]; then
+ # sed -n '/'$INSTALL_TYPE'/q;p' $HOME/ArchMitas/pkg-files/pacman-pkgs.txt | while read line
+  #do
+   # if [[ ${line} == '--END OF MINIMAL INSTALL--' ]]; then
+      # ->If selected installation type is FULL, skip the --END OF THE MINIMAL INSTALLATION-- line
+    #  continue
+    #fi
+    #echo "INSTALLING: ${line}"
+    #sudo pacman -S --noconfirm --needed ${line}
+  #done
+#fi
 echo -ne "
 -------------------------------------------------------------------------
                     Installing Microcode
